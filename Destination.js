736735b75d7e079c81e366d114282e4f68c5deb6
Balamurali950 <120172888+Balamurali950@users.noreply.github.com>
2023-07-27 13:16:30 +0530

    Add files via upload

---
+++ Destination.js
@@ -0,0 +1,114 @@
+import React from 'react'
+import {Container ,Card, Col, Button} from 'react-bootstrap';  
+// import img1 from './downloadsip.jpeg';  
+
+const Destination = () => {
+  return (
+    <>
+        <h2 class="text-center">ENJOY WONDERFULL EXPERIENCES</h2>
+        <div className="App">  
+            <Container className='p-4'>  
+                <Col md="4">  
+                <Card>  
+                    <Card.Img variant="top"  />  
+                    <Card.Body>  
+                        <Card.Title>Washington US</Card.Title>  
+                        <Card.Text>  
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Cras vitae molestie magna. Vivamus sed molestie enim,
+                         eu convallis mauris. Aliquam pharetra velit ac enim maximus,
+                          a commodo augue hendrerit. Phasellus at aliquam est  
+                          
+                        </Card.Text>  
+                        <h6>Rs 3lakhs</h6>
+                        <Button variant="">Know More</Button>  
+                    </Card.Body>  
+                </Card>  
+                </Col>  
+            </Container>  
+
+            <Container className='p-4'>  
+                <Col md="4">  
+                <Card>  
+                    <Card.Img variant="top"  />  
+                    <Card.Body>  
+                        <Card.Title>Patna</Card.Title>  
+                        <Card.Text>  
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae molestie magna. Vivamus sed molestie enim, eu convallis mauris. Aliquam pharetra velit ac enim maximus, a commodo augue hendrerit. Phasellus at aliquam est  
+                        </Card.Text> 
+                        <h6>Rs 2lakhs</h6> 
+                        <Button variant="">Know More</Button>  
+                    </Card.Body>  
+                </Card>  
+                </Col>  
+            </Container>  
+
+            <Container className='p-4'>  
+                <Col md="4">  
+                <Card>  
+                    <Card.Img variant="top"/>  
+                    <Card.Body>  
+                        <Card.Title>Kerala</Card.Title>  
+                        <Card.Text>  
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae molestie magna. Vivamus sed molestie enim, eu convallis mauris. Aliquam pharetra velit ac enim maximus, a commodo augue hendrerit. Phasellus at aliquam est  
+                        </Card.Text>  
+                        <h6>Rs 2lakhs</h6>
+                        <Button variant="">Know More</Button>  
+                    </Card.Body>  
+                </Card>  
+                </Col>  
+            </Container>  
+
+            <Container className='p-4'>  
+                <Col md="4">  
+                <Card>  
+                    <Card.Img variant="top"  />  
+                    <Card.Body>  
+                        <Card.Title>Bangladesh</Card.Title>  
+                        <Card.Text>  
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae molestie magna. Vivamus sed molestie enim, eu convallis mauris. Aliquam pharetra velit ac enim maximus, a commodo augue hendrerit. Phasellus at aliquam est  
+                        </Card.Text>  
+                        <h6>Rs 2lakhs</h6>
+                        <Button variant="">Know More</Button>  
+                    </Card.Body>  
+                </Card>  
+                </Col>  
+            </Container>  
+
+            <Container className='p-4'>  
+                <Col md="4">  
+                <Card>  
+                    <Card.Img variant="top"/>  
+                    <Card.Body>  
+                        <Card.Title>Dubai</Card.Title>  
+                        <Card.Text>  
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae molestie magna. Vivamus sed molestie enim, eu convallis mauris. Aliquam pharetra velit ac enim maximus, a commodo augue hendrerit. Phasellus at aliquam est  
+                        </Card.Text>  
+                        <h6>Rs 2lakhs</h6>
+                        <Button variant="">Know More</Button>  
+                    </Card.Body>  
+                </Card>  
+                </Col>  
+            </Container>  
+
+            <Container className='p-4'>  
+                <Col md="4">  
+                <Card>  
+                    <Card.Img variant="top"  />  
+                    <Card.Body>  
+                        <Card.Title>Manipur</Card.Title>  
+                        <Card.Text>  
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae molestie magna. Vivamus sed molestie enim, eu convallis mauris. Aliquam pharetra velit ac enim maximus, a commodo augue hendrerit. Phasellus at aliquam est  
+                        </Card.Text> 
+                        <h6>Rs 2lakhs</h6> 
+                        <Button variant="">Know More</Button>  
+                    </Card.Body>  
+                </Card>  
+                </Col>  
+            </Container>  
+        </div>  
+    </>
+  )
+}
+
+export default Destination
